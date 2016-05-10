e25e028412ab8ca61c61d2c8d66acc2023b7c441
asama-yaya <hiro.asamayaya@gmail.com>
2016-05-10 16:11:17 +0900

    test1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+// 事前に内部パラメータを取得しているならDefine
+// If you have a intrinsic parameter of camera, you should define this
+//#define PRE_CALIBRATION
+
+
+int main(int argc, char **argv){
+
+	printf("aaaaa\n");
+
+	return 0;
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <Windows.h>
+#include <math.h>
